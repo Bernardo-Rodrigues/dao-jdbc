59e9397c2e3398dcc3005d77e87cda53f33de273
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-11-30 14:25:37 -0300

    feat: add Seller class

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,12 +1,17 @@
 package application;
 
 import models.entities.Department;
+import models.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Department department = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+
+        System.out.println(seller);
     }
 }
