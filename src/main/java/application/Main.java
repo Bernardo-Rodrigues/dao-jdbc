d43ac4b3913fab9e3afb40e955db064814256a12
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-11-30 17:20:00 -0300

    feat: add update and delete in SellerDaoJDBC impl

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -7,10 +7,13 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -30,5 +33,24 @@ public class Main {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("4° Method: seller insert");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("5° Method: seller update");
+        Seller sellerToUpdate = sellerDao.findById(1);
+        sellerToUpdate.setName("Martha Waine");
+        sellerDao.update(sellerToUpdate);
+        System.out.println("Update completed");
+
+        System.out.println("6° Method: seller delete");
+        System.out.println("Enter id for delete test: ");
+        int id = scanner.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        scanner.close();
     }
 }
